0374c2f92f7711b1f0dc0a55d0918f7c73d9ee28
woniper <leekw3747@gmail.com>
2014-07-13 11:01:15 +0900

    MainMethod 작성

--- AES256/src/com/woniper/aes/MainAES.java
+++ AES256/src/com/woniper/aes/MainAES.java
@@ -2,8 +2,17 @@ package com.woniper.aes;
 
 public class MainAES {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws Exception {
+		String key = "aes256-test-key!!"; 		// key는 16자 이상 
+		AES256Util aes256 = new AES256Util(key);
+		
+		String text = "암호화되지 않은 문자입니다.";
+		String encText = aes256.aesEncode(text);
+		String decText = aes256.aesDecode(encText);
+		
+		System.out.println("암호화할 문자 : " + text);
+		System.out.println("암호화된 문자 : " + encText);
+		System.out.println("복호화된 문자 : " + decText);
 	}
 
 }
